2e28a7fc8e37760eaa656e6700d05ab41c3ffa4a
Jone.Luo <jone.luo@outlook.com>
2018-11-23 21:38:55 +0800

    update

--- base/src/main/kt/com/jone/base/json/JsonHelperProtocol.kt
+++ base/src/main/kt/com/jone/base/json/JsonHelperProtocol.kt
@@ -4,10 +4,11 @@ package com.jone.base.json
  * Created by Jone.L on 2018/3/27.
  *  Json工具类协议
  */
-interface JsonHelperProtocol {
+interface JsonHelperProtocol
+{
     fun <T> convertJsonToList(json: String, clazz: Class<T>): List<T>?
 
-    fun <T> convertJsonToObject(json: String, clazz: Class<T>): T?
+    infix fun <reified T> convertJsonToObject(json: String): T?
 
     fun convertObjectToJson(obj: Any): String?
 
